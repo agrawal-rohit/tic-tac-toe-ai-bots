837790eb934f0fc5307c7b889cc5534e6f9cb6a2
Rohit Agrawal <rohitagrawalofficialmail@gmail.com>
2018-12-14 15:54:59 +0530

    RL done

--- Tic Tac Toe/training_(AIvsAI)_ReinforcementLearning.py
+++ Tic Tac Toe/training_(AIvsAI)_ReinforcementLearning.py
@@ -2,6 +2,7 @@ import numpy as np
 from math import inf as infinity
 import itertools
 import random
+import time
 
 game_state = [[' ',' ',' '],
               [' ',' ',' '],
@@ -72,20 +73,34 @@ states_dict = {}
 all_possible_states = [[list(i[0:3]),list(i[3:6]),list(i[6:10])] for i in itertools.product(player, repeat = 9)]
 n_states = len(all_possible_states) # 2 players, 9 spaces
 n_actions = 9   # 9 spaces
-state_values_for_AI = np.full((n_states),0.0)
+state_values_for_AI_O = np.full((n_states),0.0)
+state_values_for_AI_X = np.full((n_states),0.0)
 print("n_states = %i \nn_actions = %i"%(n_states, n_actions))
 
+# State values for AI 'O'
 for i in range(n_states):
     states_dict[i] = all_possible_states[i]
     winner, _ = check_current_state(states_dict[i])
     if winner == 'O':   # AI won
-        state_values_for_AI[i] = 1
+        state_values_for_AI_O[i] = 1
     elif winner == 'X':   # AI lost
-        state_values_for_AI[i] = -1
+        state_values_for_AI_O[i] = -1
+        
+# State values for AI 'X'       
+for i in range(n_states):
+    winner, _ = check_current_state(states_dict[i])
+    if winner == 'O':   # AI lost
+        state_values_for_AI_X[i] = -1
+    elif winner == 'X':   # AI won
+        state_values_for_AI_X[i] = 1
 
-def update_state_value(curr_state_idx, next_state_idx, learning_rate):
-    new_value = state_values_for_AI[curr_state_idx] + learning_rate*(state_values_for_AI[next_state_idx]  - state_values_for_AI[curr_state_idx])
-    state_values_for_AI[curr_state_idx] = new_value
+def update_state_value_O(curr_state_idx, next_state_idx, learning_rate):
+    new_value = state_values_for_AI_O[curr_state_idx] + learning_rate*(state_values_for_AI_O[next_state_idx]  - state_values_for_AI_O[curr_state_idx])
+    state_values_for_AI_O[curr_state_idx] = new_value
+    
+def update_state_value_X(curr_state_idx, next_state_idx, learning_rate):
+    new_value = state_values_for_AI_X[curr_state_idx] + learning_rate*(state_values_for_AI_X[next_state_idx]  - state_values_for_AI_X[curr_state_idx])
+    state_values_for_AI_X[curr_state_idx] = new_value
 
 def getBestMove(state, player, epsilon):
     '''
@@ -104,7 +119,10 @@ def getBestMove(state, player, epsilon):
         new_state = copy_game_state(state)
         play_move(new_state, player, empty_cell)
         next_state_idx = list(states_dict.keys())[list(states_dict.values()).index(new_state)]
-        curr_state_values.append(state_values_for_AI[next_state_idx])
+        if player == 'X':
+            curr_state_values.append(state_values_for_AI_X[next_state_idx])
+        else:
+            curr_state_values.append(state_values_for_AI_O[next_state_idx])
         
     print('Possible moves = ' + str(moves))
     print('Move values = ' + str(curr_state_values))    
@@ -120,39 +138,42 @@ def getBestMove(state, player, epsilon):
     return best_move
 
 # PLaying
+
+#LOAD TRAINED STATE VALUES
+state_values_for_AI_X = np.loadtxt('trained_state_values_X.txt', dtype=np.float64)
+state_values_for_AI_O = np.loadtxt('trained_state_values_O.txt', dtype=np.float64)
+
 learning_rate = 0.2
 epsilon = 0.2
-play_again = 'Y'
-while play_again == 'Y' or play_again == 'y':
+num_iterations = 10000
+for iteration in range(num_iterations):
     game_state = [[' ',' ',' '],
               [' ',' ',' '],
               [' ',' ',' ']]
     current_state = "Not Done"
-    print("\nNew Game!")
+    print("\nIteration " + str(iteration) + "!")
     print_board(game_state)
-    player_choice = input("Choose which player goes first - X (You - the petty human) or O(The mighty AI): ")
     winner = None
-    
-    if player_choice == 'X' or player_choice == 'x':
-        current_player_idx = 0
-    else:
-        current_player_idx = 1
+    current_player_idx = random.choice([0,1])
         
     while current_state == "Not Done":
         curr_state_idx = list(states_dict.keys())[list(states_dict.values()).index(game_state)]
-        if current_player_idx == 0: # Human's turn
-            block_choice = int(input("Oye Human, your turn! Choose where to place (1 to 9): "))
+        if current_player_idx == 0:     # AI_X's turn
+            print("\nAI X's turn!")         
+            block_choice = getBestMove(game_state, players[current_player_idx], epsilon)
             play_move(game_state ,players[current_player_idx], block_choice)
             new_state_idx = list(states_dict.keys())[list(states_dict.values()).index(game_state)]
             
-        else:   # AI's turn
+        else:       # AI_O's turn
+            print("\nAI O's turn!")   
             block_choice = getBestMove(game_state, players[current_player_idx], epsilon)
             play_move(game_state ,players[current_player_idx], block_choice)
             new_state_idx = list(states_dict.keys())[list(states_dict.values()).index(game_state)]
         
         print_board(game_state)
         #print('State value = ' + str(state_values_for_AI[new_state_idx]))
-        update_state_value(curr_state_idx, new_state_idx, learning_rate)
+        update_state_value_O(curr_state_idx, new_state_idx, learning_rate)
+        update_state_value_X(curr_state_idx, new_state_idx, learning_rate)
         winner, current_state = check_current_state(game_state)
         if winner is not None:
             print(str(winner) + " won!")
@@ -162,7 +183,9 @@ while play_again == 'Y' or play_again == 'y':
         if current_state is "Draw":
             print("Draw!")
             
-    play_again = input('Wanna try again BIYTACH?(Y/N) : ')
-    if play_again == 'N':
-        print('Suit yourself bitch!')
-    
+        #time.sleep(1)
+print('Training Complete!')    
+
+# Save state values for future use
+np.savetxt('trained_state_values_X.txt', state_values_for_AI_X, fmt = '%.6f')
+np.savetxt('trained_state_values_O.txt', state_values_for_AI_O, fmt = '%.6f')
